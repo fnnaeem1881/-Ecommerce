aea003cd6e1395b44114de878d7b94c88a7e28bd
fnnaeem1881 <mehedinaeem66@gmail.com>
2021-02-23 10:40:00 +0600

    echomeacrh

---
+++ assetc/js/costom.js
@@ -0,0 +1,13 @@
+if ($(window).width() > 992) {
+  $(window).scroll(function(){  
+     if ($(this).scrollTop() > 40) {
+        $('#navbar_top').addClass("fixed-top");
+        // add padding top to show content behind navbar
+        $('body').css('padding-top', $('.navbar').outerHeight() + 'px');
+      }else{
+        $('#navbar_top').removeClass("fixed-top");
+         // remove padding top from body
+        $('body').css('padding-top', '0');
+      }   
+  });
+}
